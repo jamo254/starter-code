30394d53984c461c80f5b62e748662cbd0006b28
jamo254 <70902444+jamo254@users.noreply.github.com>
2022-02-12 21:30:24 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>I am here!</h2>
+      <h2>So we are right in the building</h2>
+    </div>
+  );
+}
