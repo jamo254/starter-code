a6e6e6b35bdc648f3bbdd495436d4b8c81f4bd41
Fred Denis <fredjamo68@gmail.com>
2022-05-05 18:38:14 +0300

    Edit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>This is  starter code</h1>
       <h2>I am here!</h2>
       <h2>So we are right in the building</h2>
     </div>
